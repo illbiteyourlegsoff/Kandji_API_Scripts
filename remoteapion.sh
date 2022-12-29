536e6600a435aa0f584267f812beacaf697e3cdd
Trey Howell <42068057+illbiteyourlegsoff@users.noreply.github.com>
2022-12-29 09:07:07 -0600

    Create remoteapion.sh
    Script to add to Kandji to enable/disable Remote Desktop using Kandji Enterprise API

---
+++ remoteapion.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -x
+######This script will activate remote desktop using API
+####create a API token just for Remote Desktop and Device list and add the token below leaving in quotes
+#####once token and instance name is added just add to Custom Script and add what blueprints you want it for. 
+# this can be used to turn off remote also changing true to false. 
+
+
+##########################################Variables
+
+#####Server instance name
+instnme="trey"  ### if trey.kandji.io was your instance, you would just put trey
+
+
+
+####Enterprise API token
+token="add token here"
+
+
+#####################################################end of variables
+
+
+
+######get device serial number 
+serial=$(ioreg -l | grep IOPlatformSerialNumber | awk '{print $NF}' | sed -e 's/^"//' -e 's/"$//')
+
+
+
+
+####get this device from the device list and pull out Device UUID
+rawCall=$(curl --location --request GET 'https://'$instnme'.clients.us-1.kandji.io/api/v1/devices/?serial_number='$serial'' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer '$token'' | cut -b 16-51)
+
+
+
+
+
+####this will send the call to enable remote desktop. Change to false below if you want to disable remote desktop
+curl --location --request POST 'https://'$instnme'.clients.us-1.kandji.io/api/v1/devices/'$rawCall'/action/remotedesktop/' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer '$token'' \
+--data-raw '{
+    "EnableRemoteDesktop": true
+}'
+
+
