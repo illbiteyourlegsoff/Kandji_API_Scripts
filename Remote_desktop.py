bcf0bcfa9bc0a5580857911a4a3a78f3d743d73d
Trey Howell <42068057+illbiteyourlegsoff@users.noreply.github.com>
2023-10-05 10:11:33 -0500

    Update Remote_desktop.py
    remove extra as using global variable now

--- Remote_desktop.py
+++ Remote_desktop.py
@@ -31,37 +31,9 @@ subprocess.check_call([sys.executable, '-m','pip', 'install', 'requests'])
 
 
 
-#######################Serial number
-import json
-import subprocess
-system_profile_data = subprocess.Popen(
-    ['system_profiler', '-json', 'SPHardwareDataType'], stdout=subprocess.PIPE)
-data = json.loads(system_profile_data.stdout.read())
-serial = data.get('SPHardwareDataType', {})[0].get('serial_number')
-###serial = "C02GLrrr7"  ####option to hardcode
-######end of serial number
-
-####the Api Call to get uuid/device id
-import requests
-url = "https://" + instnme + ".clients.us-1.kandji.io/api/v1/devices/?serial_number=" + serial
-
-payload={}
-headers = {
-     'Authorization': 'Bearer ' + token
- }
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-
-
-####make variable of just the uuid
-devid = response.text[15:51]
-#########end of api call to get UUID/device id
-
-
 ################Api Call to set remote
 
-url2 = "https://" + instnme + ".clients.us-1.kandji.io/api/v1/devices/" + devid + "/action/remotedesktop/"
+url2 = "https://" + instnme + ".clients.us-1.kandji.io/api/v1/devices/" + '$DEVICE_ID' + "/action/remotedesktop/"
 
 payload2 = json.dumps({
   "EnableRemoteDesktop": False
